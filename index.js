bffe799a69a9126a6b6d502600a32b34004b3750
Bryan Medina <bryan.medina.per@gmail.com>
2023-08-05 07:00:13 -0600

    add mongoDB to notes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
@@ -37,7 +40,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
@@ -86,7 +91,7 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
